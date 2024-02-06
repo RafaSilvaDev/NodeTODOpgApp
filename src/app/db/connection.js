5399059ad76a0f703d3e70ba8a8a2feabe499785
Rafael Barbosa <rafael.barbosa@encora.com>
2024-02-06 16:04:24 -0300

    Fixes to deploy

--- src/app/db/connection.js
+++ src/app/db/connection.js
@@ -3,14 +3,7 @@ import dotenv from "dotenv";
 const { Client } = pg;
 dotenv.config();
 
-const conn = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl:true,
-});
+const conn = new Client(process.env.DB_URL);
 
 conn.connect();
 

ac3d35d6e44ee9d3c72daba090bd32f23406a327
Rafael Barbosa <rafael.barbosa@encora.com>
2024-02-06 16:35:50 -0300

    Changing structure

--- src/App.js
+++ src/App.js
@@ -6,9 +6,4 @@ app.use(express.json());
 
 app.use(router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: ${PORT}`);
-});
-
 export default app;
